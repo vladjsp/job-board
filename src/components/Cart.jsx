4dc2530e0125395276dc0859474de05bff848238
vladjsp <vladjsp@gmail.com>
2023-05-12 10:58:07 +0300

    init

--- src/components/Cart.jsx
+++
@@ -1,6 +0,0 @@
-import CartItem from './CartItem';
-
-const Cart = ({ products }) => {
-  return products.map((text, index) => <CartItem text={text} />);
-};
-export default Cart;
--- src/components/CartItem.jsx
+++
@@ -1,5 +0,0 @@
-const CartItem = ({ text }) => {
-  return;
-};
-
-export default CartItem;
